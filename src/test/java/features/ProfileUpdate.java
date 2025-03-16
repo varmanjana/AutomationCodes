7aa0cf85a2f14743d73215d1f08c806c284212de
varmaanjana <varmaanjana27@gmail.com>
2025-03-16 12:28:15 +0530

    initial files commit

---
+++ src/test/java/features/ProfileUpdate.java
@@ -0,0 +1,37 @@
+package features;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class ProfileUpdate {
+
+	public static WebDriver driver;
+	
+	public static void main(String[] args) throws InterruptedException, IOException
+	{
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+				
+		driver.get("https://www.naukri.com/mnjuser/profile?id=&altresid");
+		
+		driver.findElement(By.id("usernameField")).sendKeys("anjanaprabhu27@gmail.com");
+		driver.findElement(By.id("passwordField")).sendKeys("Papamummy618327@");
+		driver.findElement(By.cssSelector("[type='submit']")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.cssSelector(".view-profile-wrapper a")).click();
+		Thread.sleep(3000);
+		driver.findElement(By.linkText("Update")).click();
+		Thread.sleep(3000);
+		Runtime.getRuntime().exec("C:\\Users\\ASUS\\Desktop\\Resume\\Cognizant\\fileupload");
+		
+		
+	}
+
+}
