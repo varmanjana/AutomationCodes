14e727b65c54b2ac35530c15bcf904c1c619521a
varmaanjana <varmaanjana27@gmail.com>
2025-03-19 23:17:03 +0530

    commit Robot class changes

--- src/test/java/features/ProfileUpdate.java
+++ src/test/java/features/ProfileUpdate.java
@@ -1,6 +1,12 @@
 package features;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
+import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -14,11 +20,12 @@ public class ProfileUpdate {
 	public static WebDriver driver;
 	
 	@Test
-	public void updateProfile() throws InterruptedException, IOException
+	public void updateProfile() throws InterruptedException, IOException, AWTException
 	{
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
 		
 				
 		driver.get("https://www.naukri.com/mnjuser/profile?id=&altresid");
@@ -26,14 +33,25 @@ public class ProfileUpdate {
 		driver.findElement(By.id("usernameField")).sendKeys("anjanaprabhu27@gmail.com");
 		driver.findElement(By.id("passwordField")).sendKeys("Papamummy618327@");
 		driver.findElement(By.cssSelector("[type='submit']")).click();
-		Thread.sleep(3000);
 		driver.findElement(By.cssSelector(".view-profile-wrapper a")).click();
-		Thread.sleep(3000);
 		driver.findElement(By.linkText("Update")).click();
-		Thread.sleep(3000);
-		Runtime.getRuntime().exec("C:\\Users\\ASUS\\Desktop\\Resume\\Cognizant\\fileupload");
 		
-		driver.close();
+		Robot robot=new Robot();
+		robot.delay(2000);
+		StringSelection filepath = new StringSelection("C:\\Users\\ASUS\\Desktop\\Resume\\Cognizant\\Anjana Varma Resume.pdf");
+		Toolkit.getDefaultToolkit().getSystemClipboard().setContents(filepath, null);
+		
+		robot.keyPress(KeyEvent.VK_CONTROL);
+		robot.keyPress(KeyEvent.VK_V);
+		
+		robot.keyRelease(KeyEvent.VK_CONTROL);
+		robot.keyRelease(KeyEvent.VK_V);
+		
+		robot.keyPress(KeyEvent.VK_ENTER);
+		robot.keyRelease(KeyEvent.VK_ENTER);
+		
+		System.out.println("File uploaded successfully");
+		//driver.close();
 		
 		
 	}
